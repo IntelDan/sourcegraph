4ccf585d111f7d29bd43fa2416be3fd2fa1b6820
Felix Becker <felix.b@outlook.com>
2019-04-25 16:25:01 +0200

    Use browser API

--- client/browser/src/libs/code_intelligence/code_intelligence.tsx
+++ client/browser/src/libs/code_intelligence/code_intelligence.tsx
@@ -48,7 +48,6 @@ import {
     toURIWithPath,
     ViewStateSpec,
 } from '../../../../../shared/src/util/url'
-import { sendMessage } from '../../browser/runtime'
 import { isInPage } from '../../context'
 import { ERPRIVATEREPOPUBLICSOURCEGRAPHCOM } from '../../shared/backend/errors'
 import { createLSPFromExtensions, toTextDocumentIdentifier } from '../../shared/backend/lsp'
@@ -363,9 +362,7 @@ export function handleCodeHost({
             catchError(() => [false])
         )
 
-    const openOptionsMenu = () => {
-        sendMessage({ type: 'openOptionsPage' })
-    }
+    const openOptionsMenu = () => browser.runtime.sendMessage({ type: 'openOptionsPage' })
 
     const addedElements = mutations.pipe(
         concatAll(),
